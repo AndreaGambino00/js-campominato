8dbb25e8f92a0de3d9d5847eeaca7228016f0425
Andrea <andreagambinomarketing@gmail.com>
2021-07-27 19:11:19 +0200

    Non so più cosa fare

--- js/main.js
+++ js/main.js
@@ -33,13 +33,13 @@ console.log(bombs);
 
 //* Gioco
 
-var punti = 0;
-var possibilita =   (rangebombe - 16);
-var disinneschi = [];
+ var punti = 0;
+ var possibilita =   (rangebombe - 16);
+ var disinneschi = [];
 
 do {
     //Chiedere all' utente di inserire un numero tra 1 e 100
-    var scelta = Number(prompt("Inserisci un numero comreso tra 1 e " + rangebombe ));
+     var scelta = Number(prompt("Inserisci un numero comreso tra 1 e " + rangebombe ));
 
     //Verificare se input può essere inserito nell array disinneschi
     if(isNaN(scelta)){
@@ -61,6 +61,31 @@ do {
 
 } while (disinneschi.length < possibilita && !bombs.includes(scelta) )
 
-if (disinneschi.length == possibilita){
+ if (disinneschi.length == possibilita){
     alert("Complimenti sei un artificiere incredibile!")
 }
+
+
+function creaCampo(celle) {
+    for (let i = 1; i<= celle; i++) {
+        let cella = `
+        <div data-cella="${i}" class="cella">
+        `;
+
+        let templateCella = document.createElement('DIV');
+        templateCella.classList.add("quadrato");
+        templateCella.innerHTML = cella;
+        document.getElementById('campo').appendChild(templateCella);
+    }
+}
+
+document.getElementById('campo').addEventListener('click', 
+    function(e)  {
+        console.log(e.target.dataset.cella);
+        let element = document.querySelectorAll("[data-cella='" + + "']");
+        console.log(element[0]);
+        element[0].classList.add("red");
+    }
+)
+
+creaCampo(100);
